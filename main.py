0094c009f2237e79fa254726265fd77274399e84
Joao Torrinhas <joaotorrinhas@NULL.Home>
2025-03-16 14:13:39 +0000

    quick fix

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     
     if form_data.username != fake_users_db["username"] or not verify_password(form_data.password, fake_users_db["password"]):
         raise HTTPException(
-            status_code=400, detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer"}
+            status_code=401, detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer"}
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
